294727dbb7383af37b4182c7445a9ec8e187967c
Kuldeep rathor <127830786+kuldeep-rathor@users.noreply.github.com>
2023-12-20 06:19:51 +0530

    02Vite

--- 01basicreact/01vitereact/src/App.jsx
+++ 01basicreact/01vitereact/src/App.jsx
@@ -1,11 +1,12 @@
-
+import Chai from "./Chai";
 
 function App() {
-  
-
-  return (
-   <h1>Chai aur  react with vite | KD</h1>
+  return(
+<>
+<h1>Chai aur Code with vite | KD</h1>
+<Chai />
+</>
   )
 }
 
-export default App
+export default App;
---
+++ 01basicreact/01vitereact/src/Chai.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Chai = () => {
+  return (
+    <div>
+      <h2>Chai is ready .....</h2>
+    </div>
+  )
+}
+
+export default Chai
